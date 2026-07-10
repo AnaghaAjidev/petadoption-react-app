7c31f24cd4457a27c338db1439fa46dbcf63e1f4
AnaghaAjidev <anaghaajidev@gmail.com>
2026-07-10 22:35:40 +0530

    Commit new changes

--- src/components/SearchPet.jsx
+++ src/components/SearchPet.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import NavBar from './NavBar'
 
 const SearchPet = () => {
-  return (
-    <>
-    <NavBar/>
-    <div className="container mt-5">
-      <div className="card shadow p-4 col-md-6 mx-auto">
+    return (
+        <>
+            <NavBar />
+            <div className="container mt-5">
+                <div className="card shadow p-4 col-md-6 mx-auto">
 
-        <h3 className="text-center mb-3">Search Pet</h3>
+                    <h3 className="text-center mb-3">Search Pet</h3>
 
-        <label className="form-label">Pet ID</label>
-        <input
-          type="text"
-          className="form-control mb-3"
-          placeholder="Enter Pet ID"
-        />
+                    <label className="form-label">Pet ID</label>
+                    <input
+                        type="text"
+                        className="form-control mb-3"
+                        placeholder="Enter Pet ID"
+                    />
 
-        <button className="btn btn-primary">
-          Search
-        </button>
+                    <button className="btn btn-primary">
+                        Search
+                    </button>
 
-      </div>
-    </div>
-    </>
-  )
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default SearchPet
